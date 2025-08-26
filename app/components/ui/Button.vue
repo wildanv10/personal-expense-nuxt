b4d997017a5e95df3355ba9949f9ff925d0a2403
Wildan Novaldi <wildanv10@gmail.com>
2025-08-26 22:13:11 +0700

    refactor: make Button reusable

---
+++ app/components/ui/Button.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    type="button"
+    class="cursor-pointer w-full gap-2 border py-2 text-lg rounded-md flex justify-center items-center"
+  >
+    <slot></slot>
+  </button>
+</template>
