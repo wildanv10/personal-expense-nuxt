a739d5e542ced225b4d9e74732791551a8066a9c
Wildan Novaldi <wildanv10@gmail.com>
2025-08-26 22:13:01 +0700

    refactor: make Logo reusable

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -3,16 +3,7 @@
     class="sticky top-0 z-50 w-full bg-white border-b border-(--border-color)"
   >
     <div class="container flex h-16 items-center justify-between px-4">
-      <div class="flex items-center gap-2">
-        <div
-          class="flex h-8 w-8 items-center justify-center rounded-lg bg-gradient-primary"
-        >
-          <span class="text-sm font-bold text-white">PE</span>
-        </div>
-        <div>
-          <h1 class="text-xl font-heading font-semibold">Personal Expense</h1>
-        </div>
-      </div>
+      <UiLogo />
     </div>
   </header>
 </template>
---
+++ app/components/ui/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center gap-2">
+    <div
+      class="flex h-8 w-8 items-center justify-center rounded-lg bg-gradient-primary"
+    >
+      <span class="text-sm font-bold text-white">PE</span>
+    </div>
+    <div>
+      <h1 class="text-xl font-heading font-semibold">Personal Expense</h1>
+    </div>
+  </div>
+</template>
