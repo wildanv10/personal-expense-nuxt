68e4782428a570ab23108944f8bb00c46489e76a
Wildan Novaldi <wildanv10@gmail.com>
2025-08-28 00:01:40 +0700

    feat: add WIP transactions page

--- app/pages/home/transactions.vue
+++ app/pages/home/transactions.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+const { getTransactions, transactions, loading, error } = useTransactions();
+
+onMounted(async () => {
+  await getTransactions();
+});
+</script>
+
 <template>
   <div>
     <h1>Transactions</h1>
     <p>This page will be displayed at the /home/transactions route.</p>
+    <hr />
+    <p>"transactions:"{{ transactions }}</p>
+    <hr />
+    <p>"loading:"{{ loading }}</p>
+    <hr />
+    <p>"error:"{{ error }}</p>
   </div>
 </template>
