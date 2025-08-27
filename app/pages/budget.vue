5cb95228325761a9990c1be0db57d4d2e3400d1f
Wildan Novaldi <wildanv10@gmail.com>
2025-08-27 23:59:23 +0700

    chore: remove unused pages

--- app/pages/budget.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section>
-    <h1>Budget</h1>
-    <p>This page will be displayed at the /budget route.</p>
-  </section>
-</template>
--- app/pages/transactions.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section>
-    <h1>Transactions</h1>
-    <p>This page will be displayed at the /transactions route.</p>
-  </section>
-</template>
