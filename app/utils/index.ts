977b68324f6ead99ffb1ace4bc58ea2e500c251f
Wildan Novaldi <wildanv10@gmail.com>
2025-08-28 23:07:23 +0700

    feat: add constants for routes

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -7,4 +7,25 @@ export const constants = {
       { key: "income", value: "Income" },
     ],
   },
+  routes: {
+    // home page
+    home: "/home",
+    home_transactions: "/home/transactions",
+    home_budgets: "/home/budgets",
+
+    // transaction page
+    transaction: "/transaction",
+
+    // statistics page
+    statistics: "/statistics",
+
+    // settings page
+    settings: "/settings",
+    settings_categories: "/settings/categories",
+    settings_categories_add: "/settings/categories/add",
+    settings_categories_update: "/settings/categories/update",
+    settings_payment_methods: "/settings/payment-methods",
+    settings_payment_methods_add: "/settings/payment-methods/add",
+    settings_payment_methods_update: "/settings/payment-methods/update",
+  },
 };
