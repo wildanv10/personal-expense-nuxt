506afd7ae2c827bd547fdfa601596ff5523f1531
Wildan Novaldi <wildanv10@gmail.com>
2025-08-29 20:08:43 +0700

    fix: set icon gradient color

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,9 +1,18 @@
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
 <template>
-  <header
-    class="sticky top-0 z-50 w-full bg-white border-b border-(--border-color)"
-  >
-    <div class="container flex h-16 items-center justify-between px-4">
+  <header class="sticky top-0 z-50 w-full bg-white border-b border-gray-300">
+    <div class="container flex h-16 items-center justify-center px-4 relative">
       <UiLogo />
+      <span class="absolute left-0 top-0 pl-3 h-full flex items-center">
+        <UIcon
+          name="i-lucide-arrow-left"
+          class="size-6 cursor-pointer"
+          @click="router.back()"
+        />
+      </span>
     </div>
   </header>
 </template>
