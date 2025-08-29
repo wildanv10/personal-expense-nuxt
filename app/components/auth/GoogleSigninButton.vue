163a4e40f9a05131b4e2514999df79b3bf3926b5
Wildan Novaldi <wildanv10@gmail.com>
2025-08-29 19:34:45 +0700

    refactor: substitute lucide-vue-next with @iconify-json/lucide

--- app/components/auth/GoogleSigninButton.vue
+++ app/components/auth/GoogleSigninButton.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { LogIn } from "lucide-vue-next";
-
 const { signInWithGoogle } = useAuth();
 </script>
 
 <template>
   <UiButton class="bg-white border-neutral-200" @click="signInWithGoogle">
-    <LogIn :size="22" />
+    <UIcon name="i-lucide-log-in" class="size-6" />
     Sign in with Google
   </UiButton>
 </template>
