4ae0c1daff1e80357f46bc6b4547eee7af30859b
Wildan Novaldi <wildanv10@gmail.com>
2025-08-31 11:51:40 +0700

    feat(transactions): show transactions list
    - Add util function groupTransactionsByDate
    - Add util function formatWithThousandSeparator
    - Show transaction list by grouping by the date

--- app/pages/home/transactions.vue
+++ app/pages/home/transactions.vue
@@ -8,13 +8,57 @@ onMounted(async () => {
 
 <template>
   <div>
-    <h1>Transactions</h1>
-    <p>This page will be displayed at the /home/transactions route.</p>
-    <hr />
-    <p>"transactions:"{{ transactions }}</p>
-    <hr />
-    <p>"loading:"{{ loading }}</p>
-    <hr />
-    <p>"error:"{{ error }}</p>
+    <h1 class="font-semibold">Transactions</h1>
+
+    <div
+      class="mt-2"
+      v-for="(date, id) in groupTransactionsByDate(transactions)"
+      :key="id"
+    >
+      <div class="card flex flex-col gap-3">
+        <h1 class="font-semibold text-xs text-gray-500 mb-2">
+          {{ formatDate(id) }}
+        </h1>
+        <div
+          class="flex items-center justify-between gap-3"
+          v-for="transaction in date"
+          :key="transaction.id"
+        >
+          <div class="flex items-center space-x-4">
+            <div
+              class="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center"
+            >
+              <UIcon
+                v-if="transaction.sub_categories?.icon"
+                :name="`i-lucide-${transaction.sub_categories?.icon}`"
+                size="16"
+              />
+            </div>
+            <div>
+              <p class="font-semibold text-gray-800">
+                {{ transaction.sub_categories?.name }}
+              </p>
+              <p class="text-xs text-gray-500">
+                {{ transaction.description }}
+              </p>
+            </div>
+          </div>
+          <div class="text-right">
+            <p
+              class="font-semibold text-gray-900"
+              :class="{
+                'text-red-500': transaction.type === constants.expense,
+                'text-green-600': transaction.type === constants.income,
+              }"
+            >
+              <span v-if="transaction.type === constants.expense">-</span>
+              <span v-else-if="transaction.type === constants.income">+</span>
+              {{ formatWithThousandSeparator(transaction.amount) }}
+            </p>
+            <p class="text-xs text-gray-500">&nbsp;</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
---
+++ app/utils/transactions.ts
@@ -0,0 +1,45 @@
+import type { TransactionWithRelations } from "~/types/transactions";
+
+/**
+ * Format a number with a thousand separator.
+ * @param {number | string} number - The number to format.
+ * @param {string} [locale='en-US'] - The locale to use for formatting (default is 'en-US').
+ * @returns {string} The formatted number with thousand separators.
+ */
+export function formatWithThousandSeparator(
+  number: number | string,
+  locale: string = "id-ID"
+): string {
+  // Ensure the number is parsed as a float
+  const parsedNumber = parseFloat(number as string);
+
+  // Check if the input is a valid number
+  if (isNaN(parsedNumber)) {
+    throw new Error("Invalid number");
+  }
+
+  // Format the number using the provided locale
+  return new Intl.NumberFormat(locale).format(parsedNumber);
+}
+
+/**
+ * Utility function to group transactions by date.
+ * @param transactions - Array of transaction objects.
+ * @returns - Grouped transactions by date.
+ */
+export function groupTransactionsByDate(
+  transactions: TransactionWithRelations[]
+): Record<string, TransactionWithRelations[]> {
+  // Sort date descending
+
+  return transactions
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .reduce((groups, transaction) => {
+      const date = transaction.date;
+      if (!groups[date]) {
+        groups[date] = [];
+      }
+      groups[date].push(transaction);
+      return groups;
+    }, {} as Record<string, TransactionWithRelations[]>);
+}
